147f569fb271930b69b2466aa4b9381a2857a139
Adri?n Bernardi <adrian.bernardi@gmail.com>
2019-10-10 19:14:04 -0300

    lims_sale: improvements
    * Sale Clauses
    * Automatic email to Party
    * Invoice Party
    * Purchase order Number
    * Expiration date
    * Product type, Matrix and Service (analysis) in Sale line

---
+++ lims_sale/configuration.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+# This file is part of lims_sale module for Tryton.
+# The COPYRIGHT file at the top level of this repository contains
+# the full copyright notices and license terms.
+
+from trytond.model import ModelSQL, ModelView, fields
+from trytond.pool import PoolMeta
+
+__all__ = ['Configuration', 'Clause']
+
+
+class Configuration(metaclass=PoolMeta):
+    __name__ = 'sale.configuration'
+
+    email_quotation_subject = fields.Char('Subject of the quotation email')
+    email_quotation_body = fields.Text('Body of the quotation email')
+
+
+class Clause(ModelSQL, ModelView):
+    'Clause'
+    __name__ = 'sale.clause'
+
+    name = fields.Char('Name', required=True)
+    description = fields.Text('Description', required=True)
---
+++ lims_sale/party.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# This file is part of lims_sale module for Tryton.
+# The COPYRIGHT file at the top level of this repository contains
+# the full copyright notices and license terms.
+
+from trytond.model import fields
+from trytond.pool import PoolMeta
+
+__all__ = ['Party']
+
+
+class Party(metaclass=PoolMeta):
+    __name__ = 'party.party'
+
+    purchase_order_required = fields.Boolean('Purchase order required')
+    internal_code = fields.Char('Internal Code')
