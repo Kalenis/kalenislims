c8495b87b926bc99d12d948fdfce4cf32b6e2007
Mario Puntin <mariomop@gmail.com>
2020-02-13 19:18:18 -0300

    lims_interface: module initial commit

---
+++ lims_interface/function.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# This file is part of lims_interface module for Tryton.
+# The COPYRIGHT file at the top level of this repository contains
+# the full copyright notices and license terms.
+
+
+def concat(*args):
+    return ''.join([a if isinstance(a, str) else '' for a in args])
+
+custom_functions = {}
+custom_functions['CONCAT'] = concat
