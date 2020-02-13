ce4570974dfefc0649a148ed24f02b685b5ca1f1
Adri?n Bernardi <adrian.bernardi@gmail.com>
2020-02-13 09:29:31 -0300

    lims_analysis_sheet: add module for Analysis sheets

---
+++ lims_analysis_sheet/configuration.py
@@ -0,0 +1,53 @@
+# This file is part of lims_analysis_sheet module for Tryton.
+# The COPYRIGHT file at the top level of this repository contains
+# the full copyright notices and license terms.
+
+from trytond.model import fields
+from trytond.pyson import Eval
+from trytond.pool import Pool, PoolMeta
+
+__all__ = ['Configuration', 'ConfigurationSequence']
+
+
+class Configuration(metaclass=PoolMeta):
+    __name__ = 'lims.configuration'
+
+    analysis_sheet_sequence = fields.MultiValue(fields.Many2One(
+        'ir.sequence', 'Analysis Sheet Sequence', required=True,
+        domain=[
+            ('company', 'in',
+                [Eval('context', {}).get('company', -1), None]),
+            ('code', '=', 'lims.analysis_sheet'),
+            ]))
+
+    @classmethod
+    def default_analysis_sheet_sequence(cls, **pattern):
+        return cls.multivalue_model(
+            'analysis_sheet_sequence').default_analysis_sheet_sequence()
+
+    @classmethod
+    def multivalue_model(cls, field):
+        pool = Pool()
+        if field == 'analysis_sheet_sequence':
+            return pool.get('lims.configuration.sequence')
+        return super(Configuration, cls).multivalue_model(field)
+
+
+class ConfigurationSequence(metaclass=PoolMeta):
+    __name__ = 'lims.configuration.sequence'
+
+    analysis_sheet_sequence = fields.Many2One('ir.sequence',
+        'Analysis Sheet Sequence', depends=['company'], domain=[
+            ('company', 'in', [Eval('company', -1), None]),
+            ('code', '=', 'lims.analysis_sheet'),
+            ])
+
+    @classmethod
+    def default_analysis_sheet_sequence(cls):
+        pool = Pool()
+        ModelData = pool.get('ir.model.data')
+        try:
+            return ModelData.get_id('lims.analysis_sheet',
+                'seq_analysis_sheet')
+        except KeyError:
+            return None
---
+++ lims_analysis_sheet/interface.py
@@ -0,0 +1,163 @@
+# This file is part of lims_analysis_sheet module for Tryton.
+# The COPYRIGHT file at the top level of this repository contains
+# the full copyright notices and license terms.
+
+from trytond.model import Workflow, ModelView, ModelSQL, fields, Unique
+from trytond.wizard import Wizard, StateAction
+from trytond.pool import Pool
+from trytond.pyson import PYSONEncoder, Eval, Bool
+from trytond.transaction import Transaction
+
+__all__ = ['TemplateAnalysisSheet', 'TemplateAnalysisSheetAnalysis',
+    'AnalysisSheet', 'OpenAnalysisSheetData']
+
+
+class TemplateAnalysisSheet(ModelSQL, ModelView):
+    'Analysis Sheet Template'
+    __name__ = 'lims.template.analysis_sheet'
+
+    interface = fields.Many2One('lims.interface', 'Device Interface',
+        required=True, domain=[('state', '=', 'active')],
+        states={'readonly': Bool(Eval('interface'))})
+    name = fields.Char('Name', required=True)
+    analysis = fields.One2Many('lims.template.analysis_sheet.analysis',
+        'template', 'Analysis', required=True)
+    max_qty_samples = fields.Integer('Maximum quantity of samples',
+        help='For generation from racks')
+    comments = fields.Text('Comments')
+
+    @fields.depends('interface')
+    def on_change_with_name(self, name=None):
+        if self.interface:
+            return self.interface.name
+
+
+class TemplateAnalysisSheetAnalysis(ModelSQL, ModelView):
+    'Template Analysis'
+    __name__ = 'lims.template.analysis_sheet.analysis'
+
+    template = fields.Many2One('lims.template.analysis_sheet', 'Template',
+        required=True, ondelete='CASCADE', select=True)
+    analysis = fields.Many2One('lims.analysis', 'Analysis',
+        required=True, select=True)
+    method = fields.Many2One('lims.lab.method', 'Method')
+
+
+class AnalysisSheet(Workflow, ModelSQL, ModelView):
+    'Analysis Sheet'
+    __name__ = 'lims.analysis_sheet'
+
+    template = fields.Many2One('lims.template.analysis_sheet', 'Template',
+        required=True, readonly=True)
+    compilation = fields.Many2One('lims.interface.compilation', 'Compilation',
+        required=True, readonly=True)
+    laboratory = fields.Many2One('lims.laboratory', 'Laboratory')
+    professional = fields.Many2One('lims.laboratory.professional',
+        'Professional', required=True)
+    samples_qty = fields.Function(fields.Integer('Samples Qty.'),
+        'get_samples_qty')
+    number = fields.Char('Number', readonly=True)
+    date = fields.Function(fields.DateTime('Date'), 'get_date')
+    state = fields.Selection([
+        ('draft', 'Draft'),
+        ('active', 'Active'),
+        ], 'State', required=True, readonly=True)
+    planification = fields.Many2One('lims.planification', 'Planification',
+        readonly=True)
+
+    @classmethod
+    def __setup__(cls):
+        super(AnalysisSheet, cls).__setup__()
+        t = cls.__table__()
+        cls._sql_constraints += [
+            ('compilation_uniq', Unique(t, t.compilation),
+                'lims_analysis_sheet.msg_sheet_compilation_unique'),
+            ]
+        cls._transitions |= set((
+            ('draft', 'active'),
+            ))
+        cls._buttons.update({
+            'view_data': {
+                'invisible': Eval('state') == 'draft',
+                },
+            'activate': {
+                'invisible': Eval('state') != 'draft',
+                },
+            })
+
+    @staticmethod
+    def default_state():
+        return 'draft'
+
+    def get_date(self, name):
+        return self.compilation.date_time
+
+    @classmethod
+    def get_samples_qty(cls, sheets, name):
+        result = {}
+        for s in sheets:
+            result[s.id] = 0
+        return result
+
+    @classmethod
+    def create(cls, vlist):
+        vlist = cls.set_number(vlist)
+        return super(AnalysisSheet, cls).create(vlist)
+
+    @classmethod
+    def set_number(cls, vlist):
+        pool = Pool()
+        Config = pool.get('lims.configuration')
+        Sequence = pool.get('ir.sequence')
+
+        config = Config(1)
+        if not config.analysis_sheet_sequence:
+            return vlist
+
+        vlist = [x.copy() for x in vlist]
+        for values in vlist:
+            number = Sequence.get_id(config.analysis_sheet_sequence.id)
+            values['number'] = number
+        return vlist
+
+    @classmethod
+    @ModelView.button
+    @Workflow.transition('active')
+    def activate(cls, sheets):
+        Compilation = Pool().get('lims.interface.compilation')
+        Compilation.activate([s.compilation for s in sheets])
+
+    @classmethod
+    @ModelView.button_action(
+        'lims_analysis_sheet.wiz_open_analysis_sheet_data')
+    def view_data(cls, sheets):
+        pass
+
+
+class OpenAnalysisSheetData(Wizard):
+    'Open Analysis Sheet Data'
+    __name__ = 'lims.analysis_sheet.open_data'
+
+    start = StateAction('lims_interface.act_open_compilation_data')
+
+    def do_start(self, action):
+        AnalysisSheet = Pool().get('lims.analysis_sheet')
+
+        context = {
+            'lims_interface_compilation': None,
+            'lims_interface_table': None,
+            }
+        domain = [('compilation', '=', None)]
+        name = ''
+
+        sheet_id = Transaction().context.get('active_id', None)
+        if sheet_id:
+            sheet = AnalysisSheet(sheet_id)
+            context['lims_interface_compilation'] = sheet.compilation.id
+            context['lims_interface_table'] = sheet.compilation.table.id
+            domain = [('compilation', '=', sheet.compilation.id)]
+            name = ' (%s)' % sheet.number
+        action['pyson_context'] = PYSONEncoder().encode(context)
+        action['pyson_domain'] = PYSONEncoder().encode(domain)
+        action['name'] += name
+        return action, {}
