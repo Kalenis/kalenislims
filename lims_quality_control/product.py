0820b3eb56dd54ea271ab4a6b17376a3427cfe67
Sebastián Marró <smarro@kalenislims.com>
2020-04-11 18:19:16 -0300

    lims_quality_control: Add module

---
+++ lims_quality_control/product.py
@@ -0,0 +1,14 @@
+# This file is part of lims_quality_control module for Tryton.
+# The COPYRIGHT file at the top level of this repository contains
+# the full copyright notices and license terms.
+from trytond.model import fields
+from trytond.pool import PoolMeta
+
+__all__ = ['Template']
+
+
+class Template(metaclass=PoolMeta):
+    __name__ = 'product.template'
+
+    product_type = fields.Many2One('lims.product.type', 'Product type')
+    matrix = fields.Many2One('lims.matrix', 'Matrix')
