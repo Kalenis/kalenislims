c8177ec44f0c20384ee67d5678934cf26239de1a
Adrián Bernardi <abernardi@kalenislims.com>
2020-05-05 12:41:21 -0300

    lims_diagnosis: add Diagnosis States

--- lims_diagnosis/html_template.py
+++ lims_diagnosis/html_template.py
@@ -2,10 +2,11 @@
 # The COPYRIGHT file at the top level of this repository contains
 # the full copyright notices and license terms.
 
-from trytond.model import ModelSQL, ModelView, fields
+from trytond.model import ModelSQL, ModelView, fields, DictSchemaMixin
 from trytond.pool import PoolMeta
 
-__all__ = ['DiagnosisTemplate', 'ReportTemplate']
+__all__ = ['DiagnosisState', 'DiagnosisTemplate', 'DiagnosisTemplateState',
+    'ReportTemplate']
 
 
 class DiagnosisTemplate(ModelSQL, ModelView):
@@ -14,6 +15,29 @@ class DiagnosisTemplate(ModelSQL, ModelView):
 
     name = fields.Char('Name', required=True)
     content = fields.Text('Content', required=True)
+    diagnosis_states = fields.Many2Many(
+        'lims.diagnosis.template-diagnosis.state',
+        'template', 'state', 'States')
+
+
+class DiagnosisState(DictSchemaMixin, ModelSQL, ModelView):
+    'Diagnosis State'
+    __name__ = 'lims.diagnosis.state'
+
+    @staticmethod
+    def default_type_():
+        return 'selection'
+
+
+class DiagnosisTemplateState(ModelSQL):
+    'Diagnosis Template - State'
+    __name__ = 'lims.diagnosis.template-diagnosis.state'
+    _table = 'lims_diagnosis_template_diagnosis_state'
+
+    template = fields.Many2One('lims.diagnosis.template', 'Template',
+        required=True, ondelete='CASCADE', select=True)
+    state = fields.Many2One('lims.diagnosis.state', 'State',
+        required=True, ondelete='CASCADE', select=True)
 
 
 class ReportTemplate(metaclass=PoolMeta):
