98b012e68bdee00c502d627d7f27384fb7067a06
Adrián Bernardi <abernardi@kalenislims.com>
2020-05-09 16:53:31 -0300

    lims_diagnosis: Results Report: add state Diagnosed

--- lims_diagnosis/results_report.py
+++ lims_diagnosis/results_report.py
@@ -2,7 +2,7 @@
 # The COPYRIGHT file at the top level of this repository contains
 # the full copyright notices and license terms.
 
-from trytond.model import fields
+from trytond.model import ModelView, fields
 from trytond.pool import PoolMeta
 from trytond.pyson import Eval
 
@@ -17,3 +17,22 @@ class ResultsReportVersionDetail(metaclass=PoolMeta):
     diagnosis_template = fields.Many2One('lims.diagnosis.template',
         'Diagnosis Template', depends=['state'],
         states={'readonly': Eval('state') != 'draft'})
+
+    @classmethod
+    def __setup__(cls):
+        super(ResultsReportVersionDetail, cls).__setup__()
+        diagnosed_state = ('diagnosed', 'Diagnosed')
+        if diagnosed_state not in cls.state.selection:
+            cls.state.selection.append(diagnosed_state)
+        cls._buttons.update({
+            'diagnose': {
+                'invisible': Eval('state') != 'draft',
+                'depends': ['state'],
+                },
+            })
+        cls._buttons['revise']['invisible'] = Eval('state') != 'diagnosed'
+
+    @classmethod
+    @ModelView.button
+    def diagnose(cls, details):
+        cls.write(details, {'state': 'diagnosed'})
