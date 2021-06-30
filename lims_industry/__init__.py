24530eb579b11477385c74d9d817e5789d7fa703
Adrián Bernardi <abernardi@kalenislims.com>
2021-06-30 12:12:50 -0300

    lims_industry: add variables to format results report name

--- lims_industry/__init__.py
+++ lims_industry/__init__.py
@@ -44,6 +44,7 @@ def register():
         results_report.ResultsReportVersionDetail,
         results_report.ResultsReportVersionDetailSample,
         results_report.ResultsReportVersionDetailLine,
+        results_report.ReportNameFormat,
         party.Party,
         party.Address,
         task.AdministrativeTaskTemplate,
@@ -52,6 +53,7 @@ def register():
         module='lims_industry', type_='model')
     Pool.register(
         configuration.Configuration,
+        results_report.ResultsReport2,
         module='lims_industry', type_='model',
         depends=['lims_email'])
     Pool.register(
