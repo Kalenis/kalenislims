e364ff885c709e178fab3ba3a31683ed2b492583
Adrián Bernardi <abernardi@kalenislims.com>
2021-11-01 13:23:35 -0300

    lims, lims_interface, lims_analysis_sheet: improves performance in sheets     validation/confirmation

--- lims_interface/interface.py
+++ lims_interface/interface.py
@@ -2004,6 +2004,8 @@ class Compilation(Workflow, ModelSQL, ModelView):
         Data = pool.get('lims.interface.data')
         Field = pool.get('lims.interface.table.field')
 
+        return  # TODO: check values and correct type
+
         for c in compilations:
             fields = {}
             columns = Field.search([
