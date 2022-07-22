206ff197d658117675250e4dcbe4e826265f37c0
Adrián Bernardi <abernardi@kalenislims.com>
2022-07-22 18:36:53 -0300

    lims: notebook: set result modifier when calculating IR

--- lims/notebook.py
+++ lims/notebook.py
@@ -2614,8 +2614,10 @@ class NotebookInternalRelationsCalc1(Wizard):
             notebook_line = notebook_lines[0]
             if result is not None:
                 notebook_line.result = str(result)
+                notebook_line.result_modifier = 'eq'
             if converted_result is not None:
                 notebook_line.converted_result = str(converted_result)
+                notebook_line.converted_result_modifier = 'eq'
             if result is not None or converted_result is not None:
                 notebook_line.start_date = date
                 notebook_line.end_date = date
@@ -3124,8 +3126,10 @@ class NotebookInternalRelationsCalc2(Wizard):
             notebook_line = notebook_lines[0]
             if result is not None:
                 notebook_line.result = str(result)
+                notebook_line.result_modifier = 'eq'
             if converted_result is not None:
                 notebook_line.converted_result = str(converted_result)
+                notebook_line.converted_result_modifier = 'eq'
             if result is not None or converted_result is not None:
                 notebook_line.start_date = date
                 notebook_line.end_date = date
