268aa4d6baeb1ff2f730fe98b2c87510f5537e40
Adrián Bernardi <abernardi@kalenislims.com>
2022-08-16 20:17:08 -0300

    lims: notebook line: allow to extend list of custom views

--- lims/notebook.py
+++ lims/notebook.py
@@ -1342,6 +1342,11 @@ class NotebookLine(ModelSQL, ModelView):
         return [('analysis',) + tuple(clause[1:])]
 
     @classmethod
+    def _get_custom_views(cls):
+        # All Notebook Lines view
+        return ['notebook_line_all_list']
+
+    @classmethod
     def fields_view_get(cls, view_id=None, view_type='form', level=None):
         pool = Pool()
         User = pool.get('res.user')
@@ -1350,8 +1355,7 @@ class NotebookLine(ModelSQL, ModelView):
 
         result = super().fields_view_get(view_id, view_type, level)
 
-        # All Notebook Lines view
-        if view_id and UiView(view_id).name == 'notebook_line_all_list':
+        if view_id and UiView(view_id).name in cls._get_custom_views():
             return result
 
         notebook_view = User(Transaction().user).notebook_view
