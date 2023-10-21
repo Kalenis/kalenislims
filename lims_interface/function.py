ad33fbe9c6c874cd460dec62fba1b2cb18f1eda7
Adrián Bernardi <abernardi@kalenislims.com>
2023-10-21 20:17:38 -0300

    lims_interface: add NOW_STR function

--- lims_interface/function.py
+++ lims_interface/function.py
@@ -7,6 +7,9 @@ import formulas
 import numpy as np
 from dateutil.relativedelta import relativedelta
 from decimal import Decimal
+import datetime
+from dateutil.relativedelta import relativedelta
+import pytz
 
 from trytond.model import ModelView, ModelSQL, fields
 from trytond.pool import Pool
@@ -295,6 +298,24 @@ def min_date(dates):
 
 
 custom_functions['MINDATE'] = min_date
+
+
+def now_str():
+    pool = Pool()
+    Company = pool.get('company.company')
+
+    timezone = pytz.utc
+
+    company_id = Transaction().context.get('company')
+    if company_id:
+        company = Company(company_id)
+        if company.timezone:
+            timezone = pytz.timezone(company.timezone)
+    now = datetime.datetime.now(timezone)
+    return now.strftime("%d/%m/%Y %H:%M:%S")
+
+
+custom_functions['NOW_STR'] = now_str
 
 
 def slope(yp, xp):
