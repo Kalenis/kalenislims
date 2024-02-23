0f432feb89d0e3c8a6c6434b4863af9260f534ae
Pablo <pablopadulles@gmail.com>
2024-02-23 17:37:44 -0300

    About

--- lims/__init__.py
+++ lims/__init__.py
@@ -42,6 +42,7 @@ def register():
         configuration.LabWorkYearShift,
         configuration.Cron,
         configuration.Sequence,
+        configuration.About,
         laboratory.LaboratoryProfessional,
         laboratory.Laboratory,
         laboratory.LabMethod,
