2e9805f8a480f8a2cd01bdfebb5b28f2897346fc
Adrián Bernardi <abernardi@kalenislims.com>
2024-02-28 20:44:45 -0300

    lims: samples pending reporting: filter allowed laboratories

--- lims/results_report.py
+++ lims/results_report.py
@@ -2481,11 +2481,10 @@ class OpenSamplesPendingReportingStart(ModelView):
     __name__ = 'lims.samples_pending_reporting.start'
 
     laboratory = fields.Many2One('lims.laboratory', 'Laboratory',
-        required=True)
-
-    @staticmethod
-    def default_laboratory():
-        return Transaction().context.get('laboratory', None)
+        required=True, domain=[('id', 'in', Eval('laboratory_domain'))],
+        depends=['laboratory_domain'])
+    laboratory_domain = fields.One2Many('lims.laboratory',
+        None, 'Laboratory domain')
 
 
 class OpenSamplesPendingReporting(Wizard):
@@ -2498,6 +2497,25 @@ class OpenSamplesPendingReporting(Wizard):
             Button('Open', 'open_', 'tryton-ok', default=True),
             ])
     open_ = StateAction('lims.act_lims_samples_pending_reporting')
+
+    def default_start(self, fields):
+        pool = Pool()
+        User = pool.get('res.user')
+        Laboratory = pool.get('lims.laboratory')
+
+        res = {
+            'laboratory': Transaction().context.get('laboratory', None),
+            'laboratory_domain': [],
+            }
+
+        user = User(Transaction().user)
+        if user.laboratory:
+            res['laboratory_domain'] = [user.laboratory.id]
+        elif user.laboratories:
+            res['laboratory_domain'] = [l.id for l in user.laboratories]
+        else:
+            res['laboratory_domain'] = [l.id for l in Laboratory.search([])]
+        return res
 
     def do_open_(self, action):
         laboratory = self.start.laboratory
