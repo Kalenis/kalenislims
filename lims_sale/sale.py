f896265bff6a3ccddd3c461f65398729ea9df611
Adrián Bernardi <abernardi@kalenislims.com>
2024-03-14 11:06:31 -0300

    lims_sale, lims_sale_industry: hide tabs and buttons in wizard to modify header

--- lims_sale/sale.py
+++ lims_sale/sale.py
@@ -119,7 +119,7 @@ class Sale(metaclass=PoolMeta):
 
     @classmethod
     def view_attributes(cls):
-        return super().view_attributes() + [
+        attributes = super().view_attributes() + [
             ('//group[@id="links"]/link[@name="sale.act_shipment_form"]',
                 'states', {
                     'invisible': Eval('invoice_method') == 'service',
@@ -133,6 +133,13 @@ class Sale(metaclass=PoolMeta):
                     'invisible': Eval('state') != 'draft',
                     }),
             ]
+        if Transaction().context.get('modify_header'):
+            attributes.extend([
+                ('//group[@id="lims_buttons"]', 'states', {'invisible': True}),
+                ('//page[@id="lims_report"]', 'states', {'invisible': True}),
+                ('//page[@id="lims_email"]', 'states', {'invisible': True}),
+                ])
+        return attributes
 
     @staticmethod
     def default_services_completed_manual():
