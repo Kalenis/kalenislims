b86e594d5333dc41eb31fbf1020b0e524f024947
Adrián Bernardi <abernardi@kalenislims.com>
2025-04-09 16:12:30 -0300

    lims_analysis_sheet: improves performance when annul notebook lines

--- lims_analysis_sheet/notebook.py
+++ lims_analysis_sheet/notebook.py
@@ -169,6 +169,9 @@ class NotebookLine(metaclass=PoolMeta):
         Data = pool.get('lims.interface.data')
 
         for nb_line in nb_lines:
+            if nb_line.analysis_sheet:
+                continue
+
             template_id = nb_line.get_analysis_sheet_template()
             if not template_id:
                 continue
